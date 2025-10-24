a612e58cee35845918b31a82c2f90a4d33885611
burak <darendeburak@gmail.com>
2025-10-24 11:36:36 +0300

    Remove custom cursor utilities and initialization
    Deleted cursorUtils.js and removed its initialization from _app.jsx. This simplifies the app by eliminating custom cursor logic and related side effects.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,5 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
-import { initCustomCursors } from "../utils/cursorUtils";
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Initialize custom cursors when app loads
-    initCustomCursors();
-  }, []);
-
   return <Component {...pageProps} />;
 }
